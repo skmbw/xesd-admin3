3333a67ba561c05f83d864c51d5461467001d43e
skmbw <tongku2008@126.com>
2021-01-27 17:13:40 +0800

    班级管理基本功能

--- src/app/routes/classes/list/list.component.ts
+++ src/app/routes/classes/list/list.component.ts
@@ -1,27 +1,32 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc/st';
+import { STColumn, STComponent, STData } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
 import { ClassesEditComponent } from '../edit/edit.component';
+import { ClassesService } from '../../../shared/service/classes.service';
+import { com } from '@shared';
+import { ToastrService } from 'ngx-toastr';
+import Classes = com.xueershangda.tianxun.classroom.model.Classes;
+import ClassesReply = com.xueershangda.tianxun.classroom.model.ClassesReply;
 
 @Component({
   selector: 'app-classes-list',
   templateUrl: './list.component.html',
 })
 export class ClassesListComponent implements OnInit {
-  url = `/user`;
+  url: STData[] = [];
   searchSchema: SFSchema = {
     properties: {
-      no: {
+      name: {
         type: 'string',
-        title: '编号'
+        title: '班级名'
       }
     }
   };
   @ViewChild('st', { static: false }) st: STComponent;
   columns: STColumn[] = [
     { title: '编号', index: 'no' },
-    { title: '调用次数', type: 'number', index: 'callNo' },
+    { title: '班级名', index: 'name' },
     { title: '头像', type: 'img', width: '50px', index: 'avatar' },
     { title: '时间', type: 'date', index: 'updatedAt' },
     {
@@ -33,9 +38,26 @@ export class ClassesListComponent implements OnInit {
     }
   ];
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
+  constructor(private http: _HttpClient, private modal: ModalHelper,
+              private classesService: ClassesService, private toastr: ToastrService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const classes = new Classes();
+    classes.pageSize = 10;
+    this.load(classes);
+  }
+
+  load(classes: Classes) {
+    this.classesService.list(classes).subscribe(result => {
+      const uint8Array = new Uint8Array(result, 0, result.byteLength);
+      const reply = ClassesReply.decode(uint8Array);
+      if (reply.code === 1) {
+        this.url = reply.data as STData[];
+      } else {
+        this.toastr.success(reply.message);
+      }
+    });
+  }
 
   add() {
     this.modal
--- src/app/shared/service/classes.service.ts
+++ src/app/shared/service/classes.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { CommonService } from './common.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { com, JsUtils } from '@shared';
+import Classes = com.xueershangda.tianxun.classroom.model.Classes;
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +12,27 @@ export class ClassesService extends CommonService {
 
   constructor(private httpClient: HttpClient) {
     super(httpClient);
+  }
+
+  list(classes: Classes): Observable<ArrayBuffer> {
+    return this.postProtobuf('classes/list', this.encodeClasses(classes));
+  }
+
+  add(classes: Classes): Observable<ArrayBuffer> {
+    if (JsUtils.isNotBlank(classes.id)) {
+      return this.postProtobuf('classes/update', this.encodeClasses(classes));
+    } else {
+      return this.postProtobuf('classes/doAdd', this.encodeClasses(classes));
+    }
+  }
+
+  delete(classes: Classes): Observable<ArrayBuffer> {
+    return this.postProtobuf('classes/delete', this.encodeClasses(classes));
+  }
+
+  get(id: string): Observable<ArrayBuffer> {
+    const classes = new Classes();
+    classes.id = id;
+    return this.postProtobuf('classes/detail', this.encodeClasses(classes));
   }
 }
