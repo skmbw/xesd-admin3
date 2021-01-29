09801adc2d192e7d1f6b0fec69c68ce13a512fdf
skmbw <tongku2008@126.com>
2021-01-29 17:30:50 +0800

    课程list和新增课程业务实现

--- src/app/routes/course/edit/edit.component.ts
+++ src/app/routes/course/edit/edit.component.ts
@@ -4,18 +4,22 @@ import { Location } from '@angular/common';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { _HttpClient } from '@delon/theme';
 import { SFSchema, SFUISchema } from '@delon/form';
+import { com, JsUtils } from '@shared';
+import { CourseService } from '../../../shared/service/course.service';
+import Course = com.xueershangda.tianxun.classroom.model.Course;
+import CourseReply = com.xueershangda.tianxun.classroom.model.CourseReply;
 
 @Component({
   selector: 'app-course-edit',
   templateUrl: './edit.component.html',
 })
 export class CourseEditComponent implements OnInit {
-
+  title = '新增课程';
   id = this.route.snapshot.params.id;
   i: any;
   schema: SFSchema = {
     properties: {
-      no: { type: 'string', title: '编号' },
+      name: { type: 'string', title: '课程名' },
       owner: { type: 'string', title: '姓名', maxLength: 15 },
       callNo: { type: 'number', title: '调用次数' },
       href: { type: 'string', title: '链接', format: 'uri' },
@@ -28,8 +32,8 @@ export class CourseEditComponent implements OnInit {
       spanLabelFixed: 100,
       grid: { span: 12 },
     },
-    $no: {
-      widget: 'text'
+    $name: {
+      widget: 'string'
     },
     $href: {
       widget: 'string',
@@ -44,12 +48,23 @@ export class CourseEditComponent implements OnInit {
     private route: ActivatedRoute,
     public location: Location,
     private msgSrv: NzMessageService,
-    public http: _HttpClient,
+    public http: _HttpClient, private courseService: CourseService
   ) {}
 
   ngOnInit(): void {
-    if (this.id > 0)
-    this.http.get(`/user/${this.i.id}`).subscribe(res => (this.i = res));
+    if (JsUtils.isNotBlank(this.id)) {
+      this.courseService.get(this.id).subscribe(result => {
+        const uint8Array = new Uint8Array(result, 0, result.byteLength);
+        const reply = CourseReply.decode(uint8Array);
+        if (reply.code === 1) {
+          this.i = reply.data[0];
+        } else {
+          this.msgSrv.success(reply.message);
+        }
+      });
+    } else {
+      this.i = new Course();
+    }
   }
 
   save(value: any) {
--- src/app/routes/course/list/list.component.ts
+++ src/app/routes/course/list/list.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Injector, OnInit, ViewChild } from '@angular/core';
 import { _HttpClient, ModalHelper } from '@delon/theme';
-import { STColumn, STComponent } from '@delon/abc/st';
+import { STColumn, STComponent, STData } from '@delon/abc/st';
 import { SFSchema } from '@delon/form';
 import { CourseEditComponent } from '../edit/edit.component';
+import { CourseService } from '../../../shared/service/course.service';
+import { com } from '@shared';
+import { ToastrService } from 'ngx-toastr';
+import { DA_SERVICE_TOKEN, ITokenService } from '@delon/auth';
+import Course = com.xueershangda.tianxun.classroom.model.Course;
+import CourseReply = com.xueershangda.tianxun.classroom.model.CourseReply;
 
 @Component({
   selector: 'app-course-list',
   templateUrl: './list.component.html',
 })
 export class CourseListComponent implements OnInit {
-  url = `/user`;
+  url: STData[] = [];
   searchSchema: SFSchema = {
     properties: {
-      no: {
+      name: {
         type: 'string',
-        title: '编号'
+        title: '课程名'
       }
     }
   };
   @ViewChild('st', { static: false }) st: STComponent;
   columns: STColumn[] = [
-    { title: '编号', index: 'no' },
+    { title: '课程名', index: 'name' },
     { title: '调用次数', type: 'number', index: 'callNo' },
     { title: '头像', type: 'img', width: '50px', index: 'avatar' },
     { title: '时间', type: 'date', index: 'updatedAt' },
@@ -33,9 +39,24 @@ export class CourseListComponent implements OnInit {
     }
   ];
 
-  constructor(private http: _HttpClient, private modal: ModalHelper) { }
+  constructor(private http: _HttpClient, private modal: ModalHelper,
+              private courseService: CourseService, private message: ToastrService,
+              private injector: Injector) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    const course = new Course();
+    course.pageSize = 20;
+    course.userId = (this.injector.get(DA_SERVICE_TOKEN) as ITokenService).get().id;
+    this.courseService.list(course).subscribe(result => {
+      const uint8Array = new Uint8Array(result, 0, result.byteLength);
+      const reply = CourseReply.decode(uint8Array);
+      if (reply.code === 1) {
+        this.url = reply.data as STData[];
+      } else {
+        this.message.success(reply.message);
+      }
+    });
+  }
 
   add() {
     this.modal
