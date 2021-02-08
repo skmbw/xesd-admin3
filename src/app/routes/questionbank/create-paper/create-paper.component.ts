51486cdd8038198ea7b6e8397413020a4f08c2d5
skmbw <tongku2008@126.com>
2021-02-08 09:11:47 +0800

    创建试卷

--- src/app/routes/questionbank/create-paper/create-paper.component.ts
+++ src/app/routes/questionbank/create-paper/create-paper.component.ts
@@ -3,17 +3,22 @@ import { NzModalRef } from 'ng-zorro-antd/modal';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { _HttpClient } from '@delon/theme';
 import { SFSchema, SFUISchema } from '@delon/form';
+import { PaperService } from '../../../shared/service/paper.service';
+import { com, JsUtils } from '@shared';
+import PaperReply = com.xueershangda.tianxun.classroom.model.PaperReply;
+import Paper = com.xueershangda.tianxun.classroom.model.Paper;
 
 @Component({
   selector: 'app-questionbank-create-paper',
   templateUrl: './create-paper.component.html',
 })
 export class QuestionbankCreatePaperComponent implements OnInit {
+  title = '新增试卷';
   record: any = {};
   i: any;
   schema: SFSchema = {
     properties: {
-      no: { type: 'string', title: '编号' },
+      id: { type: 'string', title: '编号' },
       owner: { type: 'string', title: '姓名', maxLength: 15 },
       callNo: { type: 'number', title: '调用次数' },
       href: { type: 'string', title: '链接', format: 'uri' },
@@ -26,7 +31,7 @@ export class QuestionbankCreatePaperComponent implements OnInit {
       spanLabelFixed: 100,
       grid: { span: 12 },
     },
-    $no: {
+    $id: {
       widget: 'text'
     },
     $href: {
@@ -41,18 +46,36 @@ export class QuestionbankCreatePaperComponent implements OnInit {
   constructor(
     private modal: NzModalRef,
     private msgSrv: NzMessageService,
-    public http: _HttpClient,
+    public http: _HttpClient, private paperService: PaperService
   ) {}
 
   ngOnInit(): void {
-    if (this.record.id > 0)
-    this.http.get(`/user/${this.record.id}`).subscribe(res => (this.i = res));
+    if (JsUtils.isNotBlank(this.i.id)) {
+      this.paperService.get(this.i.id).subscribe(result => {
+        const uint8Array = new Uint8Array(result, 0, result.byteLength);
+        const reply = PaperReply.decode(uint8Array);
+        if (reply.code === 1) {
+          this.i = reply.data[0] as Paper;
+        } else {
+          this.msgSrv.success(reply.message);
+        }
+      });
+      this.title = '修改试卷';
+    } else {
+      this.i = new Paper();
+    }
   }
 
   save(value: any) {
-    this.http.post(`/user/${this.record.id}`, value).subscribe(res => {
-      this.msgSrv.success('保存成功');
-      this.modal.close(true);
+    this.paperService.add(value as Paper).subscribe(result => {
+      const uint8Array = new Uint8Array(result, 0, result.byteLength);
+      const reply = PaperReply.decode(uint8Array);
+      if (reply.code === 1) {
+        this.msgSrv.success('新增试卷保存成功。');
+        this.modal.close(true);
+      } else {
+        this.msgSrv.success(reply.message);
+      }
     });
   }
 
