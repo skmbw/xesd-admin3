9305124a1cb7dd769705ce1451491fafe1129960
skmbw <tongku2008@126.com>
2021-02-24 10:46:18 +0800

    新增试卷，添加科目和年级

--- src/app/routes/questionbank/create-paper/create-paper.component.ts
+++ src/app/routes/questionbank/create-paper/create-paper.component.ts
@@ -5,8 +5,15 @@ import { _HttpClient } from '@delon/theme';
 import { SFSchema, SFUISchema } from '@delon/form';
 import { PaperService } from '../../../shared/service/paper.service';
 import { com, JsUtils } from '@shared';
+import { Observable } from 'rxjs';
+import { ClassesService } from '../../../shared/service/classes.service';
+import { CourseService } from '../../../shared/service/course.service';
 import PaperReply = com.xueershangda.tianxun.classroom.model.PaperReply;
 import Paper = com.xueershangda.tianxun.classroom.model.Paper;
+import Subject = com.xueershangda.tianxun.classroom.model.Subject;
+import GradeReply = com.xueershangda.tianxun.classroom.model.GradeReply;
+import Grade = com.xueershangda.tianxun.classroom.model.Grade;
+import SubjectReply = com.xueershangda.tianxun.classroom.model.SubjectReply;
 
 @Component({
   selector: 'app-questionbank-create-paper',
@@ -19,10 +26,11 @@ export class QuestionbankCreatePaperComponent implements OnInit {
   schema: SFSchema = {
     properties: {
       id: { type: 'string', title: '编号' },
-      owner: { type: 'string', title: '姓名', maxLength: 15 },
-      callNo: { type: 'number', title: '调用次数' },
-      href: { type: 'string', title: '链接', format: 'uri' },
-      description: { type: 'string', title: '描述', maxLength: 140 },
+      name: { type: 'string', title: '试卷名', maxLength: 30 },
+      remark: { type: 'string', title: '描述' },
+      score: { type: 'number', title: '分数' },
+      gradeId: { type: 'string', title: '年级' },
+      subjectId: { type: 'string', title: '科目' },
     },
     required: ['owner', 'callNo', 'href', 'description'],
   };
@@ -34,18 +42,61 @@ export class QuestionbankCreatePaperComponent implements OnInit {
     $id: {
       widget: 'text'
     },
-    $href: {
+    $name: {
       widget: 'string',
     },
-    $description: {
+    $remark: {
       widget: 'textarea',
       grid: { span: 24 },
+    },
+    $gradeId: {
+      widget: 'select',
+      asyncData: () =>
+        new Observable(subscriber => {
+          const grade = new Grade();
+          grade.pageSize = 20;
+          this.classesService.gradeList(grade).subscribe(result => {
+            const uint8Array = new Uint8Array(result, 0, result.byteLength);
+            const reply = GradeReply.decode(uint8Array);
+            if (reply.code === 1) {
+              const gradeList = [];
+              let i = 0;
+              for (const g of reply.data) {
+                gradeList[i++] = {label: g.name, value: g.name};
+              }
+              subscriber.next([{label: '年级', group: true, children: gradeList}]);
+            }
+          });
+        })
+    },
+    $subjectId: {
+      widget: 'select',
+      asyncData: () =>
+        new Observable(subscriber => {
+          const subject = new Subject();
+          subject.pageSize = 20;
+          this.courseService.querySubjectList(subject).subscribe(result => {
+            const uint8Array = new Uint8Array(result, 0, result.byteLength);
+            const reply = SubjectReply.decode(uint8Array);
+            if (reply.code === 1) {
+              const subjectList = [];
+              let i = 0;
+              for (const g of reply.data) {
+                subjectList[i++] = { label: g.remark, value: g.remark };
+              }
+              subscriber.next([{ label: '科目', group: true, children: subjectList }]);
+            }
+          });
+        })
+    },
+    $score: {
+      widget: 'number',
     },
   };
 
   constructor(
-    private modal: NzModalRef,
-    private msgSrv: NzMessageService,
+    private modal: NzModalRef, private courseService: CourseService,
+    private msgSrv: NzMessageService, private classesService: ClassesService,
     public http: _HttpClient, private paperService: PaperService
   ) {}
 
