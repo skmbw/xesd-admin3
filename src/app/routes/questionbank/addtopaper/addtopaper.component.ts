b369a9f32d6166bbcbc24589b2216dc8fafb1c79
skmbw <tongku2008@126.com>
2021-02-24 17:06:54 +0800

    给试卷选择题目，先查询题目list

--- src/app/routes/questionbank/addtopaper/addtopaper.component.ts
+++ src/app/routes/questionbank/addtopaper/addtopaper.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { _HttpClient } from '@delon/theme';
 import { PaperService } from '../../../shared/service/paper.service';
 import { com, JsUtils } from '@shared';
 import { NzModalRef } from 'ng-zorro-antd/modal';
+import { STColumn, STComponent, STData } from '@delon/abc/st';
+import { SFSchema } from '@delon/form';
+import { QuestionBankService } from '../../../shared/service/question-bank.service';
 import Paper = com.xueershangda.tianxun.classroom.model.Paper;
 import PaperReply = com.xueershangda.tianxun.classroom.model.PaperReply;
+import QuestionBank = com.xueershangda.tianxun.classroom.model.QuestionBank;
+import QuestionBankReply = com.xueershangda.tianxun.classroom.model.QuestionBankReply;
 
 @Component({
   selector: 'app-questionbank-addtopaper',
@@ -15,9 +20,38 @@ export class QuestionbankAddtopaperComponent implements OnInit {
   title = '选择题目';
   i: any;
 
+  url: STData[] = [];
+  searchSchema: SFSchema = {
+    properties: {
+      id: {
+        type: 'string',
+        title: '编号'
+      },
+      title: {
+        type: 'string',
+        title: '题目'
+      }
+    }
+  };
+  @ViewChild('st', { static: false }) st: STComponent;
+  columns: STColumn[] = [
+    { title: '编号', index: 'id', type: 'checkbox' },
+    { title: '题目', index: 'title' },
+    { title: '题目类型', index: 'type', type: 'enum', enum: { 1: '选择题', 2: '填空题', 3: '解答题' }},
+    // { title: '所属年级', index: 'gradeId' },
+    // { title: '所属科目', index: 'subjectId' },
+    // { title: '作者', index: 'author' },
+    { title: '更新时间', index: 'updateDate', type: 'date' },
+    {
+      title: '',
+      buttons: [
+      ]
+    }
+  ];
+
   constructor(
     private modal: NzModalRef,
-    public msgSrv: NzMessageService,
+    public msgSrv: NzMessageService, private questionBankService: QuestionBankService,
     public http: _HttpClient, private paperService: PaperService
   ) { }
 
@@ -33,8 +67,20 @@ export class QuestionbankAddtopaperComponent implements OnInit {
           this.msgSrv.success(reply.message);
         }
       });
-    } else {
-      this.i = new Paper();
+
+      const questionBank = new QuestionBank();
+      questionBank.pageSize = 20;
+      this.questionBankService.list(questionBank).subscribe(result => {
+        const uint8Array = new Uint8Array(result, 0, result.byteLength);
+        const reply = QuestionBankReply.decode(uint8Array);
+        if (reply.code === 1) {
+          this.url = reply.data;
+        } else {
+          this.msgSrv.success(reply.message);
+        }
+      });
+    } else { // id不可能为空
+      this.msgSrv.success('试卷id不能为空。');
     }
   }
 }
